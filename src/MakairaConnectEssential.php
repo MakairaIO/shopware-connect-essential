e6782b5bc3e01b71d9fb0467c335d1ef11756333
BenjaminJoergerMakaira <benjamin.joerger@makaira.io>
2025-04-23 12:31:15 +0200

    fix: add dedicated logging

--- src/MakairaConnectEssential.php
+++ src/MakairaConnectEssential.php
@@ -8,10 +8,36 @@ use MakairaConnectEssential\Utils\PluginConfig;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\DirectoryLoader;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class MakairaConnectEssential extends Plugin
 {
     public const PLUGIN_VERSION = '1.1.2';
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $locator = new FileLocator('Resources/config');
+
+        $resolver = new LoaderResolver([
+            new YamlFileLoader($container, $locator),
+            new GlobFileLoader($container, $locator),
+            new DirectoryLoader($container, $locator),
+        ]);
+
+        $configLoader = new DelegatingLoader($resolver);
+
+        $confDir = \rtrim($this->getPath(), '/') . '/Resources/config';
+
+        $configLoader->load($confDir . '/{packages}/*.yml', 'glob');
+    }
 
     public function activate(ActivateContext $activateContext): void
     {
